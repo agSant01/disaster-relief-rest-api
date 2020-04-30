5d0d67c5311beeaf74671f46c32ade446bd00dad
Gabriel Santiago <gabriel.santiago16@upr.edu>
2020-04-30 19:48:38 -0400

    Added preliminary schema and default data

--- schema/populate_schema.sql
+++ schema/populate_schema.sql
@@ -1,5 +1,15 @@
-insert into mytest1(string1) values('hi1');
-insert into mytest2(string2) values('hi2');
-insert into mytest1(string1) values('hi3');
-insert into mytest2(string2) values('hi4');
-insert into mytest1(string1) values('hi5');
+-- insert admin
+INSERT INTO users (email, password) VALUES (
+  'system@admin.com',
+  crypt('password123', gen_salt('bf'))
+);
+
+-- insert senatorial regions
+insert into senate_region(senate_region_name) values('I - San Juan');
+insert into senate_region(senate_region_name) values('II - Bayamon');
+insert into senate_region(senate_region_name) values('III Arecibo');
+insert into senate_region(senate_region_name) values('IV - Mayagüez-Aguadilla');
+insert into senate_region(senate_region_name) values('V - Ponce');
+insert into senate_region(senate_region_name) values('VI - Guayama');
+insert into senate_region(senate_region_name) values('VI - Humacao');
+insert into senate_region(senate_region_name) values('VII - Carolina');
--- schema/schema_definition.sql
+++ schema/schema_definition.sql
@@ -1,11 +1,42 @@
-create table mytest1 (
-    id serial,
-    string1 varchar(20),
-    Primary key(id)
+CREATE EXTENSION pgcrypto;
+
+create table senate_region (
+    senate_region_id SERIAL NOT NULL,
+    senate_region_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (senate_region_id)
 );
 
-create table mytest2 (
-    id serial,
-    string2 varchar(20),
-    Primary key(id)
+create table city (
+    cityid serial not null,
+    city_name varchar(40) not null,
+    city_abbreviation varchar(3) NOT NULL,
+    city_senate_region integer not null,
+    foreign key (city_senate_region) references senate_region(senate_region_id),
+    primary key (cityid)
 );
+
+create table country (
+    country_id SERIAL NOT NULL,
+    contry_name VARCHAR(40) NOT NULL
+);
+
+create table users_table (
+    userid serial not null,
+    username varchar(20) not null,
+    user_password TEXT not null,
+    first_name varchar(40) not null,
+    last_name varchar(40) not null, 
+    date_of_birth date not null,
+    street1 varchar(60) not null,
+    street2 varchar(60) not null,
+    city integer not null,
+    zip_code integer not null,
+    country integer not null, 
+    gender varchar(10) not null,
+    email varchar(80) not null UNIQUE,
+    is_enabled boolean not null,
+    creation_timestamp timestamptz default localtimestamp not null,
+    primary key (userid),
+    foreign key (city) references city(cityid),
+    foreign key (country) references country(country_id)
+); 
