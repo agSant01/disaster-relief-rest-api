31a7b02e67a17532ae31ddb8f60af0448937c19a
Gabriel <gabriel.santiago16@upr.edu>
2020-04-30 15:08:47 -0400

    Put everything related to schema under schema folder

---
+++ schema/populate_schema.sql
@@ -0,0 +1,5 @@
+insert into mytest1(string1) values('hi1');
+insert into mytest2(string2) values('hi2');
+insert into mytest1(string1) values('hi3');
+insert into mytest2(string2) values('hi4');
+insert into mytest1(string1) values('hi5');
---
+++ schema/schema_definition.sql
@@ -0,0 +1,11 @@
+create table mytest1 (
+    id serial,
+    string1 varchar(20),
+    Primary key(id)
+);
+
+create table mytest2 (
+    id serial,
+    string2 varchar(20),
+    Primary key(id)
+);
