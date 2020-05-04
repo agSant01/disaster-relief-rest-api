08087e2bf4dea40b65826b59a7d40ca464424f90
Gabriel Santiago <gabriel.santiago16@upr.edu>
2020-05-04 00:20:33 -0400

    Merge remote-tracking branch 'origin/luis' into gabriel

--- routes/user/handlers.js
+++ routes/user/handlers.js
@@ -1,113 +1,63 @@
+const db = require('../../database');
+const querylib = require('./queries');
+
 exports.getRoles = (req, res, next) => {
-    let msg = {
-        roles: [
-            'System Administrator',
-            'System Manager',
-            'Supplier',
-            'Requestor',
-            'Organization Admin',
-            'Organization Representative'
-        ]
-    };
-    res.json(msg).end();
-};
-
-exports.getRequests = (req, res, next) => {
-    let msg = {
-        requests: {
-            userID: req.params.id,
-            requests: []
+    const query = {
+        text: querylib.qRoles,
+      }
+      // callback
+      db.query(query, (qerr, qres) => {
+        if (qerr) {
+            res.json(qerr.stack).end();
+        } else {
+            res.json(qres.rows).end();
         }
-    };
-    res.json(msg).end();
+      })
 };
 
-exports.getUsers = (req, res, next) => {
-    let response = {
-        users: [
-            {
-                user_id: 123,
-                first_name: 'Juan',
-                last_name: 'Del Pueblo',
-                dob: '15/05/1985',
-                address: {
-                    street_1: 'Bo. Flores, Calle Rosas 46',
-                    street_2: null,
-                    city: 'Yabucoa',
-                    country: {
-                        name: 'Puerto Rico',
-                        abbreviation: 'PR'
-                    },
-                    zip_code: '00045'
-                },
-                phone: {
-                    area_code: '787',
-                    line_number: '5555555'
-                },
-                username: 'juandelpueblo',
-                email: 'juan@pueblo.com',
-                gender: 'male',
-                user_type: 'Administrator'
-            },
-            {
-                user_id: 190,
-                first_name: 'Marcelo',
-                last_name: 'Rios',
-                dob: '15/05/1985',
-                address: {
-                    street_1: 'Bo. Caraizo, Calle Ramirez 5',
-                    street_2: null,
-                    city: 'Coamo',
-                    country: {
-                        name: 'Puerto Rico',
-                        abbreviation: 'PR'
-                    },
-                    zip_code: '00395'
-                },
-                phone: {
-                    area_code: '787',
-                    line_number: '5555555'
-                },
-                username: 'marcelo78',
-                email: 'marcelo@gmail.com',
-                gender: 'Male',
-                user_type: 'Organization Representative'
-            }
-        ]
-    };
-
-    res.json(response).end();
+exports.getAllUsers = (req, res, next) => {
+    const query = {
+        text: querylib.qAllUsers,
+      }
+      // callback
+      db.query(query, (qerr, qres) => {
+        if (qerr) {
+            res.json(qerr.stack).end();
+        } else {
+            res.json(qres.rows).end();
+        }
+      })
 };
 
 exports.getUser = (req, res, next) => {
     let user_id = req.params.id;
 
-    let response = {
-        user: {
-            user_id: user_id,
-            first_name: 'Juan',
-            last_name: 'Del Pueblo',
-            dob: '15/05/1985',
-            address: {
-                street_1: 'Bo. Flores, Calle Rosas 46',
-                street_2: null,
-                city: 'Yabucoa',
-                country: {
-                    name: 'Puerto Rico',
-                    abbreviation: 'PR'
-                },
-                zip_code: '00045'
-            },
-            phone: {
-                area_code: '787',
-                line_number: '5555555'
-            },
-            username: 'juandelpueblo',
-            email: 'juan@pueblo.com',
-            gender: 'male',
-            user_type: 'admin'
+    const query = {
+        text: querylib.qUser,
+        values: [user_id],
+      }
+      // callback
+      db.query(query, (qerr, qres) => {
+        if (qerr) {
+            res.json(qerr.stack).end();
+        } else {
+            res.json(qres.rows[0]).end();
         }
-    };
+      })
+};
+exports.getRequests = (req, res, next) => {
+    let user_id = req.params.id;
 
-    res.json(response).end();
+    const query = {
+        text: querylib.qRequestsFromUser,
+        values: [user_id],
+      }
+      // callback
+      db.query(query, (qerr, qres) => {
+        if (qerr) {
+            res.json(qerr.stack).end();
+        } else {
+            res.json(qres.rows[0]).end();
+        }
+      })
 };
--- routes/user/route.js
+++ routes/user/route.js
@@ -3,13 +3,14 @@ var handler = require('./handlers');
 var router = express.Router();
 
 // Get all users, only for Admin and System Manager
-router.get('/', handler.getUsers);
+router.get('/', handler.getAllUsers);
+
+// Get available roles
+router.get('/roles', handler.getRoles);
 
 // Get specific user information
 router.get('/:id', handler.getUser);
 
-// Get available roles
-router.get('/roles', handler.getRoles);
 
 // Get Requests submited by user
 router.get('/:id/requests', handler.getRequests);
