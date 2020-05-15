9fbf9a774052e99d4877b723adaaa8fda21e921a
Gabriel Santiago <gabriel.santiago16@upr.edu>
2020-05-15 18:02:53 -0400

    Implemented weekly statistics.

--- routes/statistics/handlers.js
+++ routes/statistics/handlers.js
@@ -2,17 +2,20 @@ const db = require('../../database');
 const querylib = require('./queries');
 
 exports.getStatisticsWeekly = (req, res, next) => {
-    let msg = {
-        data: {
-            title: '',
-            dataFormat: '',
-            data: {
-                x: [],
-                y: [],
-            },
-        },
-    };
-    res.json(msg).end();
+    db.query(querylib.qGetWeeklyStatistics)
+        .then((result) => {
+            const msg = {
+                weekly_statistics: result.rows,
+            };
+            res.json(msg).end();
+        })
+        .catch((err) => {
+            console.log(err.stack);
+
+            res.status(503)
+                .json({ error: qerr.stack })
+                .end();
+        });
 };
 
 exports.getStatisticsDaily = (req, res, next) => {
--- routes/statistics/queries.js
+++ routes/statistics/queries.js
@@ -19,10 +19,11 @@ module.exports = {
             sum(resource.resource_quantity) as requested_quantity
         from resource
         natural join request
+        natural join requested_resources
         natural join resource_type
         natural join senate_region
         where resource_status_id = 1 
-        and date(date_requested) = current_date
+        and date(date_requested) > current_date - interval '7 days'
         group by resource_type.resource_type_id
     order by resource_type.resource_type_name
     )
@@ -38,9 +39,9 @@ module.exports = {
                 select  
                     json_agg(row_to_json((SELECT d FROM (SELECT
                         resource_type_name as resource_type,
-                        submitted_quantity,
-                        requested_quantity,
-                        (submitted_quantity-requested_quantity) as net_difference,
+                        COALESCE (submitted_quantity, 0) as submitted_quantity,
+                        COALESCE (requested_quantity, 0) as requested_quantity,
+                        COALESCE ((submitted_quantity-requested_quantity), 0) as net_difference,
                         (
                             CASE WHEN submitted_quantity-requested_quantity > 0 THEN true
                                 ELSE false
@@ -48,6 +49,58 @@ module.exports = {
                         ) as resources_requests_is_matched
                     ) d)))
                 from requested 
-                natural join submitted
+                natural full join submitted
             ) as submits_requests_matching_today`,
+    qGetWeeklyStatistics: `with submitted as (
+        select 
+            resource_type.resource_type_id,
+            resource_type_name,
+            sum(resource.resource_quantity) as submitted_quantity
+        from resource
+        natural join submits_resource
+        natural join resource_type
+        natural join senate_region
+        where resource_status_id = 1 
+        and date(submits_resource.date_submitted) > current_date - interval '7 days'
+        group by resource_type.resource_type_id
+        order by resource_type.resource_type_name
+    ), requested as (
+        select 
+            resource_type.resource_type_id,
+            resource_type_name,
+            sum(resource.resource_quantity) as requested_quantity
+        from resource
+        natural join request
+        natural join requested_resources
+        natural join resource_type
+        natural join senate_region
+        where resource_status_id = 1 
+        and date(date_requested) > current_date - interval '7 days'
+        group by resource_type.resource_type_id
+    order by resource_type.resource_type_name
+    )
+    select (select json_agg(row_to_json((SELECT d FROM (SELECT
+                    resource_type_name as resource_type,
+                    submitted_quantity
+                ) d))) from submitted) as submitted_resources_week,
+            (select json_agg(row_to_json((SELECT d FROM (SELECT
+                    resource_type_name as resource_type,
+                    requested_quantity
+                ) d))) from requested) as resource_requests_week,
+            (
+                select  
+                    json_agg(row_to_json((SELECT d FROM (SELECT
+                        resource_type_name as resource_type,
+                        COALESCE (submitted_quantity, 0) as submitted_quantity,
+                        COALESCE (requested_quantity, 0) as requested_quantity,
+                        COALESCE ((submitted_quantity-requested_quantity), 0) as net_difference,
+                        (
+                            CASE WHEN submitted_quantity-requested_quantity > 0 THEN true
+                                ELSE false
+                            END
+                        ) as resources_requests_matched_week
+                    ) d)))
+                from requested 
+                natural full join submitted
+            ) as submits_requests_matching_week`,
 };
