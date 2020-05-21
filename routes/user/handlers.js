e982a9585b54e00e0151ca90b138649872900f34
Gabriel Santiago <gabriel.santiago16@upr.edu>
2020-05-21 18:53:30 -0400

    Added statistics dao

--- routes/user/handlers.js
+++ routes/user/handlers.js
@@ -1,5 +1,3 @@
-const db = require('../../database');
-const querylib = require('./queries');
 const { validate } = require('indicative/validator');
 const postSchemas = require('./post_schemas');
 const usersDao = require('./dao');
